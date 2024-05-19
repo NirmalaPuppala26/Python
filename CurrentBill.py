d9233c0f41d52cfae53dd8bf5c9e3679510d4a02
Nirmala Puppala <94101898+NirmalaPuppala26@users.noreply.github.com>
2024-05-19 19:37:55 -0400

    Add files via upload

---
+++ CurrentBill.py
@@ -0,0 +1,16 @@
+pmr = int(input("Enter the PMR value: "))
+cmr = int(input("Enter the CMR value: "))
+unitsConsumed = cmr-pmr
+if (unitsConsumed <=100):
+    billAmount = unitsConsumed*2
+    print("Amount: ",billAmount)
+elif (unitsConsumed <=300):
+    billAmount = unitsConsumed*3
+    print("Amount: ",billAmount)
+elif (unitsConsumed <=500):
+    billAmount = unitsConsumed*5
+    print("Amount: ",billAmount)
+elif (unitsConsumed >500):
+    billAmount = unitsConsumed*10
+    print("Amount: ",billAmount)
+    
---
+++ factorial.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+f = 1
+for i in range (2,n+1):
+    f = f*i
+print("Factoial of %d: %d"%(n,f) )
